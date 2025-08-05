9db32a23b60ea6fbbc1a41bb75a9c3a93ba120ed
sam <me@sammason.dev>
2025-08-04 23:41:27 -0400

    create cleanup fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,17 @@
 import pytest
-from typer.testing import CliRunner
+from pathlib import Path
+import shutil
+from lichen.config import Config
 
-@pytest.fixture
-def runner():
-    return CliRunner()
+
+@pytest.fixture()
+def global_cleanup():
+    config = Config()
+    output_dir = Path(config.temp_dir)
+    if output_dir.exists():
+        shutil.rmtree(output_dir)
+
+    yield output_dir
+
+    if output_dir.exists():
+        shutil.rmtree(output_dir)
