aebf86b9942c19931559f82a1eb1ac6821169dc1
sam <me@sammason.dev>
2025-08-05 00:26:07 -0400

    refactor using utils modules

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,10 @@
 from pathlib import Path
-from lichen import utils
+from lichen.utils.io import _make_dir, make_root_dir, make_temp_dir  # type: ignore[reportPrivateUsage]
 
 
 def test_make_dir(global_cleanup: Path):
     # Create directory test/ with _make_dir()
-    utils._make_dir("temp")  # type: ignore[reportPrivateUsage]
+    _make_dir("temp")
 
     # Determine if the directory exists
     dir_exists = Path("temp").is_dir()
@@ -13,7 +13,7 @@ def test_make_dir(global_cleanup: Path):
 
 def test_temp_dir(global_cleanup: Path):
     # Create directory temp/ with make_temp_dir()
-    utils.make_temp_dir()
+    make_temp_dir()
 
     # Determine if the directory exists
     dir_exists = Path("temp/").is_dir()
@@ -22,10 +22,8 @@ def test_temp_dir(global_cleanup: Path):
 
 def test_root_dir(global_cleanup: Path):
     # Create directory temp/test_project with make_root_dir()
-    utils.make_root_dir("test_project")
+    make_root_dir("test_project")
 
     # Determine if the directory exists
     dir_exists = Path("test_project/").is_dir()
     assert dir_exists == True
-
-
