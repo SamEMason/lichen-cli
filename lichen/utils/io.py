51ad8fd9564b196fabc66f1e3b7558a6ae37badc
sam <me@sammason.dev>
2025-08-07 16:49:27 -0400

    create make_file util function and test

--- lichen/utils/io.py
+++ lichen/utils/io.py
@@ -11,3 +11,8 @@ def make_dir(name: str):
             print(f"Permission denied: Unable to create '{name}'.")
         except Exception as e:
             print(f"An error has occurred: {e}")
+
+
+def make_file(filename: str, mode: str = "w", content: str = ""):
+    with open(filename, mode) as file:
+        file.write(content)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from lichen.utils.io import make_dir
+from lichen.utils.io import make_dir, make_file
 
 
 def test_make_dir(global_cleanup: Path):
@@ -9,4 +9,17 @@ def test_make_dir(global_cleanup: Path):
     # Determine if the directory exists
     dir_exists = Path("temp").is_dir()
     assert dir_exists == True
+
+
+def test_make_file(global_cleanup: Path):
+    # Create temp/ dir for test
+    make_dir("temp")
+
+    # Create `test.txt` file
+    make_file("temp/test.txt")
+
+    #Determine if the file exists
+    file_exists = Path("temp/test.txt").exists()
+    assert file_exists == True
+
 
