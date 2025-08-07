43a1ef295d58bc1f49c86ec955182ba125389a89
sam <me@sammason.dev>
2025-08-07 15:59:23 -0400

    break up service command into add and remove commands

--- lichen/cli/service.py
+++ lichen/cli/service.py
@@ -4,11 +4,12 @@ app = Typer()
 
 
 @app.command()
-def service(command: str, names: list[str]):
-    """Add or remove services from the Service Registry"""
-    if command == "add":
-        for name in names:
-            print(f"Service: {name} added!")
-    elif command == "remove":
-        for name in names:
-            print(f"Service: {name} removed!")
+def add(names: list[str]):
+    for name in names:
+        print(f"Service: {name} added!")
+
+
+@app.command()
+def remove(names: list[str]):
+    for name in names:
+        print(f"Service: {name} removed!")
