85111592138d6cbeaf3f0edc3c676a0cff7c7e2d
sam <me@sammason.dev>
2025-08-08 19:21:32 -0400

    create logic for iteratively scaffolding project structure from scaffold.toml data

--- lichen/scaffold/scaffold.py
+++ lichen/scaffold/scaffold.py
@@ -1,17 +1,30 @@
+from pathlib import Path
 from lichen.config import Config
-from lichen.utils.io import make_dir
+from lichen.utils.io import get_project_root, load_toml, make_dir, make_file
 
 
 def scaffold_project(name: str):
     config = Config()
 
+    # Project root directory
+    root_dir = get_project_root()
+
+    # Target directory  RENAME!!!!
+    target_dir = root_dir
+
     # Nest directory structure in temp/ in dev mode
     if config.mode == "dev":
-        # Create temp/ directory
-        make_dir(config.mode)
+        root_dir = root_dir / config.temp_dir / name
 
-        # Append `name` with temp/
-        name = f"{config.temp_dir}/{name}"
+    # Load file tree nodes from scaffold.toml
+    scaffold_file_path = f"{str(target_dir)}/{config.lichen_dir}/scaffold/scaffold.toml"
+    scaffolding = load_toml(scaffold_file_path)
 
-    # Create root project directory
-    make_dir(name)
+    nodes = scaffolding["default"][0]["nodes"]
+
+    # Iterate through file tree nodes and create scaffolding
+    for node in nodes:
+        if node["type"] == "file":
+            make_file(root_dir / Path(node["path"]))
+        elif node.type == "dir":
+            make_dir(root_dir / Path(node["path"]))
