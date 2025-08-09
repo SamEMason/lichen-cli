00dfea45f4f032b4a5a4738574a472697d09b18a
sam <me@sammason.dev>
2025-08-08 20:53:13 -0400

    modify scaffold filepath to create project in cwd

--- lichen/scaffold/scaffold.py
+++ lichen/scaffold/scaffold.py
@@ -9,22 +9,34 @@ def scaffold_project(name: str):
     # Project root directory
     root_dir = get_project_root()
 
+    # Current working directory
+    cwd = Path.cwd() 
+
+    print(f"Root Directory: {root_dir}")
+    print(f"Curr Directory: {cwd}")
+
     # Target directory  RENAME!!!!
-    target_dir = root_dir
+    toml_file_base = root_dir
+    print(f"Target Directory: {toml_file_base}")
 
     # Nest directory structure in temp/ in dev mode
     if config.mode == "dev":
-        root_dir = root_dir / config.temp_dir / name
+        root_dir = cwd / config.temp_dir / name
+        print(f"Root (modified): {root_dir}")
 
     # Load file tree nodes from scaffold.toml
-    scaffold_file_path = f"{str(target_dir)}/{config.lichen_dir}/scaffold/scaffold.toml"
+    scaffold_file_path = f"{str(toml_file_base)}/{config.lichen_dir}/scaffold/scaffold.toml"
     scaffolding = load_toml(scaffold_file_path)
 
     nodes = scaffolding["default"][0]["nodes"]
 
+    apply_nodes(nodes, root_dir)
+
+
+def apply_nodes(nodes: list[dict[str, str]], location: Path):
     # Iterate through file tree nodes and create scaffolding
     for node in nodes:
         if node["type"] == "file":
-            make_file(root_dir / Path(node["path"]))
-        elif node.type == "dir":
-            make_dir(root_dir / Path(node["path"]))
+            make_file(location / Path(node["path"]))
+        elif node["type"] == "dir":
+            make_dir(location / Path(node["path"]))
