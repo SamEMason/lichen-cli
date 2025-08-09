cd1104eb098954dd9cf967aa2ca7bd0c392eb9e8
sam <me@sammason.dev>
2025-08-08 23:13:06 -0400

    create Config.save method for persisting config changes

--- lichen/config.py
+++ lichen/config.py
@@ -1,10 +1,36 @@
+from lichen.utils.io import get_project_root, load_toml, write_toml
+
+
 class Config:
     def __init__(
         self, lichen_dir: str = "lichen", mode: str = "dev", temp_dir: str = "dev"
     ):
         self.mode = mode
         self.lichen_dir = lichen_dir
+        self.project_name: str | None = None
         self.temp_dir = temp_dir
 
     def get_lichen_dir(self, filepath: str):
         return f"{self.lichen_dir}/{filepath}"
+
+    def save(self, config_key: str, value: str):
+        # Set config key with inputted value
+        setattr(self, config_key, value)
+
+        # Get project root directory to write config
+        project_root = get_project_root()
+        config_location = project_root / "config.toml"
+
+        # Persist config update in config.toml
+        configs = load_toml(config_location)
+        print(configs)
+
+        # Add updated configs to dictionary
+        section = configs.setdefault(self.mode, {})
+        print(section)
+
+        section[config_key] = value
+        print(section)
+
+        # Overwrite config.toml with updated configs
+        write_toml(config_location, content=configs)
