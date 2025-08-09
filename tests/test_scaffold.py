a01706c1959f2d0f6625ec0bb5f9ce77cee8cb5f
sam <me@sammason.dev>
2025-08-09 15:42:24 -0400

    clean up scaffold test

--- tests/test_scaffold.py
+++ tests/test_scaffold.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 from lichen.config import Config
 from lichen.scaffold import scaffold_project
 from lichen.utils.io import get_project_root
@@ -8,11 +7,12 @@ config = Config()
 
 
 def test_scaffold_project_creates_project_dir():
-    # Test directory
+    # Test directory name
     test_dir = "test_project"
 
     # Run scaffold_project
     scaffold_project(test_dir)
 
-    # Determine whether root/dev/test_project was created
-    assert (get_project_root() / Path(f"dev/{test_dir}")).is_dir()
+    # Assert_project directory was created
+    filepath = get_project_root() / f"dev/{test_dir}"
+    assert filepath.is_dir()
