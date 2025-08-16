df3e6af1cb07d96c2f4804454d1e66afe87d71a8
sam <me@sammason.dev>
2025-08-16 14:28:55 -0400

    create basic sync and new commands

--- src/client_build/main.py
+++ src/client_build/main.py
@@ -1,4 +1,5 @@
-from typer import Typer
+from typer import Argument, Typer
+from typing_extensions import Annotated
 
 from core.config import CONFIG_FILENAME
 from core.context import Context
@@ -7,17 +8,29 @@ from core.utils.io import load_toml
 app = Typer()
 
 
-@app.command("version")
-def version_name():
+@app.command()
+def new(name: str):
+    print(f"Scaffolding new project: {name}...")
+
+
+@app.command()
+def sync(name: Annotated[str, Argument()] = "ALL"):
+    if name == "ALL":
+        print(f"Syncing scaffolds...")
+    else:
+        print(f"Syncing scaffold: {name}...")
+
+
+@app.command()
+def version():
     ctx = Context()
 
     if ctx.client_build_dir is None:
         raise NotADirectoryError("Client_build directory not found.")
-    
+
     path = ctx.client_build_dir / CONFIG_FILENAME
     data = load_toml(path)
 
     version = data.get("version", "unknown")
 
     print(f"Scaffold -- version {version}")
-
