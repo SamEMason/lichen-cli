bdc689951bf6e8974f84de4e90bb8fa7016fde94
sam <me@sammason.dev>
2025-08-16 14:42:11 -0400

    replace config.toml with .scaffold/meta.toml

--- src/client_build/commands/version.py
+++ src/client_build/commands/version.py
@@ -1,6 +1,5 @@
 from typer import Typer
 
-from core.config import CONFIG_FILENAME
 from core.context import Context
 from core.utils.io import load_toml
 
@@ -15,7 +14,7 @@ def version():
     if ctx.client_build_dir is None:
         raise NotADirectoryError("Client_build directory not found.")
 
-    path = ctx.client_build_dir / CONFIG_FILENAME
+    path = ctx.client_build_dir / ".scaffold" / "meta.toml"
     data = load_toml(path)
 
     version = data.get("version", "unknown")
